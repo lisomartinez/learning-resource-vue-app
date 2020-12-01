7339417aed425fe4c4631b4594d777acb869d961
Lisandro Martinez <lisandromartinez@gmail.com>
2020-12-01 13:53:37 -0300

    add delete resource

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -43,6 +43,7 @@ export default {
     return {
       resources: this.storedResources,
       addResource: this.addResource,
+      deleteResource: this.removeResource
     };
   },
   computed: {
@@ -67,6 +68,10 @@ export default {
       this.storedResources.unshift(newResource);
       this.selectedTab = 'stored-resources';
     },
+    removeResource(resId) {
+      const resIndex = this.storedResources.findIndex(res => res.id === resId);
+      this.storedResources.splice(resIndex, 1);
+    }
   },
 };
 </script>
